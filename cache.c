cf2d2a757a5306090b141f52ae17ed30875e5c9f
SamuelSantos <samueldamiaosantos@gmail.com>
2023-12-29 13:31:31 +0000

    now reading but program closes unexpectedly

--- cache.c
+++ cache.c
@@ -2,31 +2,50 @@
 #include <stdlib.h>
 #include "cache.h" // Include the header file
 
+Cache *readCSV(int *size)
+{
+    printf("About to open the file\n");
 
-Cache* readCSV(int* size) {
     FILE *file = fopen("caches_all.csv", "r");
     if (file == NULL)
     {
-        printf("Erro ao abrir ficheiro\n");
+        printf("Error opening file\n");
         return NULL;
     }
-
-    Cache* caches = malloc(sizeof(Cache) * 1000); // Allocate memory for 1000 caches
-    *size = 0; // Initialize size to 0
-
-    while (fscanf(file, "%[^;];%[^;];%[^;];%[^;];%f;%f;%[^;];%[^;];%f;%f;%[^;];%[^;];%d;%d;%d;%d\n",
-                  caches[*size].code, caches[*size].name, caches[*size].state, caches[*size].owner, &caches[*size].latitude, &caches[*size].longitude,
-                  caches[*size].kind, caches[*size].size, &caches[*size].difficulty, &caches[*size].terrain, caches[*size].status, caches[*size].hidden_date,
-                  &caches[*size].founds, &caches[*size].not_founds, &caches[*size].favourites, &caches[*size].altitude) == 16)
+    else
     {
-        (*size)++; // Increase size by 1
+        printf("File opened successfully\n");
+    }
+
+    Cache *caches = malloc(sizeof(Cache) * 1000); // Allocate memory for 1000 caches
+    *size = 0;                                    // Initialize size to 0
+
+    char line[1024];
+    while (fgets(line, sizeof(line), file)) {
+        printf("Current line: %s\n", line); // Print the current line
+
+        if (sscanf(line, "%[^;];%[^;];%[^;];%[^;];%f;%f;%[^;];%[^;];%f;%f;%[^;];%[^;];%d;%d;%d;%d",
+                   caches[*size].code, caches[*size].name, caches[*size].state, caches[*size].owner, &caches[*size].latitude, &caches[*size].longitude,
+                   caches[*size].kind, caches[*size].size, &caches[*size].difficulty, &caches[*size].terrain, caches[*size].status, caches[*size].hidden_date,
+                   &caches[*size].founds, &caches[*size].not_founds, &caches[*size].favourites, &caches[*size].altitude) == 16)
+        {
+            printf("Read cache: %s\n", caches[*size].code); // Print the code of the cache that was just read
+            (*size)++;                                      // Increase size by 1
+        }
     }
 
     fclose(file);
     return caches; // Return the pointer to the array
 }
-void displayCaches(Cache caches[], int size) {
-    for (int i = 0; i < size; i++) {
+void displayCaches(Cache caches[], int size)
+{   
+    printf("Size of caches: %d\n", size);
+    printf("About to iterate over caches\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("About to display cache %d\n", i);
+        printf("Cache code: %s\n", caches[i].code); // Print the code of the cache
+        
         printf("Code: %s\n", caches[i].code);
         printf("Name: %s\n", caches[i].name);
         printf("State: %s\n", caches[i].state);
@@ -44,5 +63,8 @@ void displayCaches(Cache caches[], int size) {
         printf("Favourites: %d\n", caches[i].favourites);
         printf("Altitude: %d\n", caches[i].altitude);
         printf("\n");
+
+        printf("Finished displaying cache %d\n", i);
     }
+    printf("Finished iterating over caches\n");
 }
--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    Cache* caches = NULL;
+    Cache *caches = NULL;
     int size = 0;
 
     int option = 0;
@@ -14,28 +14,38 @@ int main()
         printf("menu\n");
         printf("1. Ler ficheiro\n");
         printf("2. Mostrar informação\n");
-        printf("3. sair\n");
+        printf("3. Libertar memória\n");
+        printf("4. sair\n");
 
         scanf("%d", &option);
-        
+
         switch (option)
         {
         case 1:
             caches = readCSV(&size);
             break;
-        case 2: 
-            if (caches != NULL) {
+        case 2:
+            if (caches != NULL)
+            {
+                printf("About to display caches\n");
                 displayCaches(caches, size);
-            } else {
+                printf("Finished displaying caches\n");
+            }
+            else
+            {
                 printf("No caches loaded. Please load the file first.\n");
             }
             break;
         case 3:
             free(caches); // Free the memory allocated for caches
             break;
-        default: printf("Opção inválida\n");
+        default:
+            printf("Opção inválida\n");
             break;
         }
-    } while (option != 3);
+    } while (option != 4);
+
+    printf("Press any key to continue...\n");
+    getchar(); // Wait for user input
     return 0;
 }
